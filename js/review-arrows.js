be1d145d3499cc8f455cf806afee882ed040f09a
Zachmarcus7 <zmarcus@outlook.com>
2023-02-06 19:58:34 -0800

    Fixed review arrows

---
+++ js/review-arrows.js
@@ -0,0 +1,11 @@
+﻿// find the original bootstrap arrows
+var leftArrow  = document.getElementById("review-left");
+var rightArrow = document.getElementById("review-right");
+
+function leftClick() {
+    leftArrow.click();
+}
+
+function rightClick() {
+    rightArrow.click();
+}

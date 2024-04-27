fe22985f769e6f07cb04e6693d50739449d4465b
zachmarcus7 <zmarcus@outlook.com>
2024-04-27 08:16:57 -0700

    Adjusted a few styles

--- js/safari-smoothscroll.js
+++ js/safari-smoothscroll.js
@@ -3,7 +3,30 @@
 /* ------------------------------------------------------------------ */
 
 $(document).ready(function () {
-  // Add smooth scrolling to all links
+
+  // Add smooth scrolling to banner "learn more" button
+  $('#learn-more-button').on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 400, function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  // Add smooth scrolling banner down arrow
   $('#down-arrow').on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
@@ -24,4 +47,5 @@ $(document).ready(function () {
       });
     } // End if
   });
+
 });

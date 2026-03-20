ac39f2c04860665d11e766c1fa011b7089145ebe
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2026-03-20 16:34:44 -0700

    updated landing page + added new listings

--- js/review-swiper.js
+++ js/review-swiper.js
@@ -2,34 +2,12 @@
 /* Functionality for the review carousel on the home page (index.html).
 /* ------------------------------------------------------------------ */
 
-// Set variable for number of swiper slides
-let revSlides = window.innerWidth < 992 ? 1 : 2;
-
 let swiperTwo = new Swiper(".review-swiper", {
-  slidesPerView: revSlides,
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   navigation: {
     nextEl: ".rev-next",
     prevEl: ".rev-prev"
   }
-});
-
-addEventListener("resize", () => {
-  // Determine the number of slides based on window width
-  revSlides = window.innerWidth < 992 ? 1 : 2;
-
-  // Destroy the previous instance before creating a new one
-  swiperTwo.destroy(true, true);
-
-  // Reinitialize Swiper with the updated settings
-  swiperTwo = new Swiper(".review-swiper", {
-    slidesPerView: revSlides,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-      nextEl: ".rev-next",
-      prevEl: ".rev-prev"
-    }
-  });
 });
